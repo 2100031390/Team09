aaf0356408e6b35cfd27bd2c782dfe3cd0e061a6
2100031390 <110445212+2100031390@users.noreply.github.com>
2024-09-27 08:46:04 +0530

    Add files via upload

---
+++ src/components/skillsData.js
@@ -0,0 +1,34 @@
+// skillsData.js
+export const skills = {
+    javascript: {
+      name: 'JavaScript',
+      description: 'JavaScript is a programming language that allows you to implement complex features on web pages.',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      defaultLevel: 'Advanced',
+    },
+    react: {
+      name: 'React',
+      description: 'React is a JavaScript library for building user interfaces.',
+      link: 'https://reactjs.org/',
+      defaultLevel: 'Intermediate',
+    },
+    css: {
+      name: 'CSS',
+      description: 'CSS is a language used to describe the presentation of a document written in HTML or XML.',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+      defaultLevel: 'Advanced',
+    },
+    nodejs: {
+      name: 'Node.js',
+      description: "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+      link: 'https://nodejs.org/',
+      defaultLevel: 'Intermediate',
+    },
+    mongodb: {
+      name: 'MongoDB',
+      description: 'MongoDB is a NoSQL database program, using JSON-like documents with schema.',
+      link: 'https://www.mongodb.com/',
+      defaultLevel: 'Beginner',
+    },
+  };
+  
